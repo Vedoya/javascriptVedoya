6a08386910aa18c95cdb3e376f6b1dde0593d7bb
Ivan Vedoya <ivan.vedoyadj@gmail.com>
2024-06-11 18:09:49 -0300

    Agrego constructores y clases

--- main.js
+++ main.js
@@ -1,42 +1,58 @@
-function calcularIVA(precioBase){
-    const IVA = 0.21;
-    return precioBase * IVA;
-}
-
-function aplicarDescuento(precioTotal){
-    const descuento = 0.1;
-    const montoDescuento = precioTotal * descuento;
-    const precioConDescuento = precioTotal - montoDescuento;
-    return precioConDescuento;
-}
-
-function calcularPrecioFinal(){
-    let precioBase;
-    let valorValido = false;
+class Zapatillas{
+    constructor(precioBase, iva, descuento){
+        this.precioBase = precioBase;
+        this.iva = iva;
+        this.descuento = descuento;
+    }
     
-    while (!valorValido){
-        precioBase = parseFloat(prompt('Ingrese el precio base de las zapatillas:'));
+    calcularIVA(){
+        return this.precioBase * this.iva;
+        console.log(`Calculando IVA: ${iva}`);
+        return iva;
+    }
+    
+    aplicarDescuento(precioTotal){
+        const montoDescuento = precioTotal * this.descuento;
+        const precioConDescuento = precioTotal - montoDescuento;
+        console.log(`Aplicando descuento de ${this.descuento * 100}%: ${precioConDescuento}`);
+        return precioConDescuento;
+    }
+    
+    calcularPrecioFinal(){
+        let valorValido = false;
         
-        if (!isNaN(precioBase) && precioBase > 0){
-            valorValido = true;
-        } else {
-            alert('Por favor, ingrese un precio válido');
+        while(!valorValido){
+            this.precioBase = parseFloat(prompt('Ingrese el precio base de las zapatillas:'));
+            
+            if(!isNaN(this.precioBase) && this.precioBase > 0){
+                valorValido = true;
+            }else{
+                alert('Por favor, ingrese un precio valido');
+            }
         }
-    }
+
+        console.log(`Precio ingresado: ${this.precioBase}`);
         
-    const iva = calcularIVA(precioBase);
-    const precioConIVA = precioBase + iva;
-    
-    let precioFinal;
-    let mensajeValor;
-    
-    if (precioConIVA > 200000){
-        precioFinal = aplicarDescuento(precioConIVA);
-        mensajeValor = "Precio final de las zapatillas + IVA con descuento = $"
-    } else {
-        precioFinal = precioConIVA;
-        mensajeValor = "Precio final de las zapatillas + IVA = $"
+        const iva = this.calcularIVA();
+        const precioConIVA = this.precioBase + iva;
+        console.log(`Precio con IVA: ${precioConIVA}`);
+        
+        let precioFinal;
+        let mensajeValor;
+        
+        if(precioConIVA > 200000){
+            precioFinal = this.aplicarDescuento(precioConIVA);
+            mensajeValor = "Precio final de las zapatillas + IVA con descuento = $";
+        }else{
+            precioFinal = precioConIVA;
+            mensajeValor = "Precio final de las zapatillas + IVA = $";
+        }
+        
+        console.log(`${mensajeValor}${precioFinal.toFixed(2)}`);
+        alert(`${mensajeValor}${precioFinal.toFixed(2)}`);
     }
-    
-    alert(`${mensajeValor}${precioFinal.toFixed(2)}`);
 }
+
+console.log('Calculando precio final');
+const zapatillas = new Zapatillas(0, 0.21, 0.1);
+zapatillas.calcularPrecioFinal();
